1a9485a98d06a789d8752d25a034e863b52482a0
Javier Morales <jamoralese@duocuc.cl>
2024-06-14 00:10:15 -0400

    Redirección de Url

--- freelance/appfreelance/urls.py
+++ freelance/appfreelance/urls.py
@@ -5,5 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index' ),
-
+    path('registro/', views.registro, name='registro'),
+    path('login/', views.login, name='login'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('login_ingreso/', views.login_ingreso, name='login_ingreso'),
+    path('olvidocontraseña/', views.olvidocontraseña, name='olvidocontraseña'),
+     path('ingreso/', views.ingreso, name='ingreso'),
+    
+    
 ]
--- freelance/appfreelance/views.py
+++ freelance/appfreelance/views.py
@@ -6,3 +6,27 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request, 'appfreelance/index.html',context)
+
+def registro(request):
+    context={}
+    return render(request,'appfreelance/registro.html',context)
+
+def login(request):
+    context={}
+    return render(request,'appfreelance/login.html',context)
+
+def nosotros(request):
+    context={}
+    return render(request,'appfreelance/nosotros.html',context)
+
+def login_ingreso(request):
+    context={}
+    return render(request,'appfreelance/login_ingreso.html',context)
+
+def ingreso(request):
+    context={}
+    return render(request,'appfreelance/ingreso.html',context)
+
+def olvidocontraseña(request):
+    context={}
+    return render(request,'appfreelance/olvidocontraseña.html',context)
