ea9eb1139337d8c89104b055c120acf7423578f1
Mordelon <crist.penar@duocuc.cl>
2024-07-05 14:39:48 -0400

    implementacion de crud

--- freelance/appfreelance/urls.py
+++ freelance/appfreelance/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
     path('nosotros/', views.nosotros, name='nosotros'),
     path('login_ingreso/', views.login_ingreso, name='login_ingreso'),
     path('olvidocontraseña/', views.olvidocontraseña, name='olvidocontraseña'),
-     path('ingreso/', views.ingreso, name='ingreso'),
+    path('ingreso/', views.ingreso, name='ingreso'),
     
-    
+    path('appfreelance/lista_servicios', views.lista_servicios, name='lista_servicios'),
+    path('appfreelance/detalle_servicio', views.detalle_servicio, name='detalle_servicio'),
+    path('appfreelance/editar_servicio', views.crear_servicio, name='crear_servicio'),
+    path('appfreelance/edit_servicio', views.editar_servicio, name='editar_servicio'),
+    path('<int:id>/eliminar/', views.eliminar_servicio, name='eliminar_servicio'),
 ]
--- freelance/appfreelance/views.py
+++ freelance/appfreelance/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Servicio
+from .forms import ServicioForm 
 
 # Create your views here.
 
@@ -30,3 +32,38 @@ def ingreso(request):
 def olvidocontraseña(request):
     context={}
     return render(request,'appfreelance/olvidocontraseña.html',context)
+
+
+def lista_servicios(request):
+    servicios = Servicio.objects.all()
+    return render(request, 'appfreelance/lista_servicios.html', {'servicios': servicios})
+
+def detalle_servicio(request, id):
+    servicio = get_object_or_404(Servicio, id=id)
+    return render(request, 'appfreelance/detalle_servicio.html', {'servicio': servicio})
+
+def crear_servicio(request):
+    if request.method == "POST":
+        form = ServicioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_servicios')
+    else:
+        form = ServicioForm()
+    return render(request, 'appfreelance/edit_servicio.html', {'form': form})
+
+def editar_servicio(request, id):
+    servicio = get_object_or_404(Servicio, id=id)
+    if request.method == "POST":
+        form = ServicioForm(request.POST, instance=servicio)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_servicios')
+    else:
+        form = ServicioForm(instance=servicio)
+    return render(request, 'appfreelance/edit_servicio.html', {'form': form})
+
+def eliminar_servicio(request, id):
+    servicio = get_object_or_404(Servicio, id=id)
+    servicio.delete()
+    return redirect('lista_servicios')
